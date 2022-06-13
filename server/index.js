16bdecffcc1023e019bcd711b690f7fda6c4aac4
nslebruh <nslebrun1@gmail.com>
2022-06-13 14:35:34 +1000

    index.js changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
