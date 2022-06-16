145f20de133854278c8990464bedfcab0df1fa90
nslebruh <nslebrun1@gmail.com>
2022-06-17 01:56:10 +1000

    context test

--- src/NestedContextTest.tsx
+++ src/NestedContextTest.tsx
@@ -1,7 +1,39 @@
-import { useContext, createContext, useState } from "react";
+import React, { useContext, createContext, useState } from "react";
 
+const AuthContext = createContext<{auth: any, setAuth: Function} | undefined> (undefined);
 export default function NestedContextTest() {
-    const [auth, setAuth] = useState(null);
-    const AuthContext = createContext(auth);
-
+    const [auth, setAuth] = useState(1);
+    return (
+        <React.Fragment>
+            <AuthContext.Provider value={{auth, setAuth}}>
+                <Parent />
+                <Child />
+            </AuthContext.Provider>
+        </React.Fragment>
+    )
+}
+const Parent = () => {
+    return (
+        <>
+            <Child/>
+        </>
+    )
+}
+const Child = () => {
+    const context = useContext(AuthContext)
+    if (context === undefined) {
+        return (
+            <>
+                bruh
+            </>
+        )
+    } else {
+        return (
+            <>
+                {context.auth}
+                <button onClick={() => context.setAuth(context.auth++)}>add 1</button>
+            </>
+        )
+    }
+    
 }
