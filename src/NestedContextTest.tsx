8ea3626e6bd373d3f390573e7b320d944154fab5
nslebruh <nslebrun1@gmail.com>
2022-06-17 12:21:58 +1000

    test

--- src/NestedContextTest.tsx
+++ src/NestedContextTest.tsx
@@ -7,7 +7,6 @@ export default function NestedContextTest() {
         <React.Fragment>
             <AuthContext.Provider value={{auth, setAuth}}>
                 <Parent />
-                <Child />
             </AuthContext.Provider>
         </React.Fragment>
     )
@@ -29,10 +28,15 @@ const useContextCheck = (ctx: React.Context<any>) => {
 }
 const Child = () => {
     const {auth, setAuth} = useContextCheck(AuthContext)
+    const setAuth2 = (num: Number) => {
+        console.log(auth, num)
+        let x = auth;
+        setAuth(x + num)
+    }
         return (
             <>
                 {auth}
-                <button onClick={() => setAuth(auth+1)}>add 1</button>
+                <button onClick={() => setAuth2(1)}>add 1</button>
             </>
         )
     }
