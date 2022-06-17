7a40059455cbdd24ac09a9c6887463245089ad66
nslebruh <nslebrun1@gmail.com>
2022-06-17 13:16:17 +1000

    parent child component testing

--- src/NestedContextTest.tsx
+++ src/NestedContextTest.tsx
@@ -18,7 +18,7 @@ export default function NestedContextTest() {
 const Parent = ({children}: providerProps) => {
     return (
         <>
-            <Child/>
+            {children}
         </>
     )
 }
