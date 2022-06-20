4cf01f58a33197226422f208e0bf4197cf32e6eb
nslebruh <nslebrun1@gmail.com>
2022-06-20 18:32:36 +1000

    added basic auth testing

--- src/TestingApp.tsx
+++ src/TestingApp.tsx
@@ -8,21 +8,59 @@ import ContextTester from "./ContextTester";
 
 
 const Home = () => {
-    const {auth, setAuth} = useContextCheck(AuthContext)
+    //const {auth, setAuth} = useContextCheck(AuthContext)
     const socket = useContextCheck(SocketContext)
     const user = "😁";
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signup = (email: string, password: string) => {
+        socket.emit("signup", {email, password});
+    }
+    const login = (email: string, password: string) => {
+        socket.emit("signup", {email, password});
+    }
+    const signout = () => {
+        socket.emit("signout")
+    }
+
+    const signupListener = useCallback((data: any) => {
+        console.log(data)
+    }, []);
+    const loginListener = useCallback((data: any) => {
+        console.log(data)
+    }, []);
+    const signoutListener = useCallback((data: any) => {
+        console.log(data)
+    }, []);
+
+    useEffect(() => {
+        socket.on("signup", signupListener)
+        socket.on("login", loginListener)
+        socket.on("signout", signoutListener)
+        return () => {
+            socket.off("signup", signupListener)
+            socket.off("login", loginListener)
+            socket.off("signout", signoutListener)
+        }
+    }, [socket, signupListener, loginListener, signoutListener])
     return (
         <UserContext.Provider value={user}>
             <h1>
                 Home
             </h1>
-            <Link to="/channels">
-                Channels
-            </Link>
-            {auth}
-            <button type="button" onClick={() => setAuth(1+auth)}>Add 1 to auth</button>
-            <button type="button" onClick={() => console.log(socket)}>Console log socket</button>
             <br/>
+            <Link to="/channels">
+                <button>Channels</button>
+            </Link>
+            <br/>
+            <form>
+                <input type="email" placeholder="Email" name="email" onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={() => login(email, password)}>Login</button>
+                <button onClick={() => signup(email, password)}>Signup</button>
+                <button onClick={() => signout()}>Signout</button>
+            </form>
             <ContextTester />
         </UserContext.Provider>
     )
@@ -60,7 +98,7 @@ const Channels = () => {
 }
 
 const App = () => {
-    const [auth, setAuth] = useState<any | undefined>(0);
+    const [auth, setAuth] = useState<any | undefined>(null);
     const connect = useCallback(() => {
         console.log("connected to socket")
     }, []) 
@@ -68,9 +106,11 @@ const App = () => {
     const disconnect = useCallback(() => {
         console.log("disconnected from socket")
     }, []) 
+
     useEffect(() => {
         socket.on("connect", connect)
         socket.on("disconnect", disconnect)
+        
         return () => {
             socket.off("connect", connect)
             socket.off("disconnect", disconnect)
