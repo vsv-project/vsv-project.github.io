ecabc3840df0300f3c0e5bac5910964fe98d5d89
nslebruh <nslebrun1@gmail.com>
2022-06-21 14:23:42 +1000

    test

--- src/context/socket.tsx
+++ src/context/socket.tsx
@@ -1,8 +1,8 @@
 import { createContext } from "react"
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.REACT_APP_API_ENDPOINT + "/wss", {
-    transports: ["websocket"],
-    }).connect();
+export const Socket = io(process.env.REACT_APP_API_ENDPOINT + "/wss", {
+  transports: ["websocket"],
+}).connect();
 
 export const SocketContext = createContext<any | undefined>(undefined);
